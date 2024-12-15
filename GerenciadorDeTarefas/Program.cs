64bdf19f024d15c369a422c5bcc32cec80987aad
Fellipe Suke <fellipepires03@hotmail.com>
2024-12-15 16:21:42 -0400

    docker

--- GerenciadorDeTarefas/Program.cs
+++ GerenciadorDeTarefas/Program.cs
@@ -1,36 +1,11 @@
 using GerenciadorDeTarefas.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configurar o DbContext com a string de conexão
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer("Server=sukeserver.ddns.net,1433;Database=GerenciadorDeTarefas;User Id=sa;Password=avaliacaoJoao@123;TrustServerCertificate=True;"));
-
-// Configuração do Kestrel para escutar HTTP e HTTPS
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(80);  // Para HTTP
-
-    if (builder.Environment.IsDevelopment())
-    {
-        // Ambiente de Desenvolvimento - Caminho local para o certificado
-        string certificatePath = Path.Combine(builder.Environment.ContentRootPath, "Data", "certificado.pfx");
-        options.ListenAnyIP(443, listenOptions =>  // Para HTTPS
-        {
-            listenOptions.UseHttps(certificatePath, "123456789");
-        });
-    }
-    else
-    {
-        // Ambiente de Produção (Docker) - Caminho dentro do container
-        options.ListenAnyIP(443, listenOptions =>  // Para HTTPS
-        {
-            listenOptions.UseHttps("/root/.aspnet/https/certificado.pfx", "123456789");
-        });
-    }
-});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -41,6 +16,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
